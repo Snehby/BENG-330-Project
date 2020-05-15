8b80eef60ca7964f9e6cbabc461a0b1b36f2a350
Snehby <izinfany@gmail.com>
2020-05-14 20:20:13 -0400

    Added femoral artery data that was used
    https://onlinelibrary-wiley-com.mutex.gmu.edu/doi/full/10.1080/10739680590934745
    Table 1 Row 3

---
+++ femoral_data.m
@@ -0,0 +1,12 @@
+%Sneh Talwar
+%Femoral artery data
+%https://onlinelibrary-wiley-com.mutex.gmu.edu/doi/full/10.1080/10739680590934745
+%Table 1 row 3
+Cp1 = 1.416; %mmHg
+Cp2 = 7.901;
+Dp100 = 172.8;%mmHg - diameter of the vessel in a passive state at 100 mmHg
+Cmyo = 3.881; %determines the sensitivity of the VSM activation function to circumferential tension
+Ctone = 8.871;
+Ca1 = 1.499; %peak active tension
+Ca2 = 0.742; %diameter of the peak active stress normalized by the passive vessel diameter at 100 mmHg 
+Ca3 = 0.353; %width of the Gaussian normalized by Dp100.  
